7aaf243cfa9a882148ab36f8f0a7e4cedb244b09
akmxgs <amagsombol25@berkeley.edu>
2023-11-28 18:05:22 -0800

    tried to make one jsx file per website page, mostly empty except for a few comments for what we planned on displaying on that page

---
+++ my-react-app/src/LandingPage.jsx
@@ -0,0 +1,13 @@
+function LandingPage() {
+    return (
+        <>
+        // insert logo here
+        
+        // log in button
+        // create new user button
+
+        </>
+    )
+}
+
+export default LandingPage;
