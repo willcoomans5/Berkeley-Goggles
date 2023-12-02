553ef000bb2dfa80825da218e8381e84e3081852
Rachel Cortez <rach.cort@berkeley.edu>
2023-12-01 16:08:34 -0800

    css pages created for pages

--- my-react-app/src/pages/LandingPage.jsx
+++ my-react-app/src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 function LandingPage() {
     return (
         <>
+        <h1 className = "landingTitle"> BERKELEY GOGGLES </h1>
         <p><Link to="/login">Log in to your account</Link></p>
         <p><Link to="/signup">Sign up for Berkeley Goggles</Link></p>
         </>
