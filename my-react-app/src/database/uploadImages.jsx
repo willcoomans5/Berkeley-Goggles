16653819f9da1908bc29f202d75a019243874b2b
John Chang <jchang04@berkeley.edu>
2023-12-03 00:22:32 -0800

    Revert to old image upload

--- my-react-app/src/database/uploadImages.jsx
+++ my-react-app/src/database/uploadImages.jsx
@@ -1,56 +1,52 @@
-import { useState, useEffect } from "react";
+import React, { useState } from "react"
+import { firebaseApp as app } from "./firebase.jsx"
 import {
-  ref,
-  uploadBytes,
-  getDownloadURL,
-  getStorage,
-  listAll,
-  list,
-} from "firebase/storage";
-import { firebaseApp } from "./firebase";
-import { v4 } from "uuid";
+    ref as fileref, 
+    getStorage, 
+    uploadBytes,
+    getDownloadURL,
+    listAll,
+    list
+} from "@firebase/storage"
+import { v4 } from "uuid"; 
+import { getAuth } from "firebase/auth";
+import { Link } from "react-router-dom";
 
-const storage = getStorage(firebaseApp); 
+const storage = getStorage(app); 
+const auth = getAuth(app); 
 
-function UploadImages() {
-  const [imageUpload, setImageUpload] = useState(null);
-  const [imageUrls, setImageUrls] = useState([]);
+export default function UploadImages() {
 
-  const imagesListRef = ref(storage, "images/");
-  const uploadFile = () => {
-    if (imageUpload == null) return;
-    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-    uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        setImageUrls((prev) => [...prev, url]);
-      });
-    });
-  };
+    const [images, setImages] = useState(); 
 
-  useEffect(() => {
-    listAll(imagesListRef).then((response) => {
-      response.items.forEach((item) => {
-        getDownloadURL(item).then((url) => {
-          setImageUrls((prev) => [...prev, url]);
-        });
-      });
-    });
-  }, []);
+    const handleSave = async(e) => {
+        e.preventDefault();
+        upload(); 
+    }
 
-  return (
-    <div className="App">
-      <input
-        type="file"
-        onChange={(event) => {
-          setImageUpload(event.target.files[0]);
-        }}
-      />
-      <button onClick={uploadFile}> Upload Image</button>
-      {imageUrls.map((url) => {
-        return <img src={url} height="300"/>;
-      })}
-    </div>
-  );
+    function upload() {
+        const imageArray = []; 
+        const uid = auth.currentUser.uid; 
+       
+        for (let i = 0; i < images.length; i++) {
+            var path = `users/` + uid + `/${v4()}`; 
+            uploadBytes(fileref(storage, path), images[i]); 
+            imageArray.push(path); 
+        }
+    }
+
+    function displayImage(e) {
+        setImages(e.target.files)
+    }
+
+    return (
+        <div>
+            <form onSubmit = {handleSave}>
+                <h4>Upload Images</h4>
+                <input type="file" multiple onChange={displayImage} />
+                <button type="submit">Save</button>
+                <p><Link to="/questions"><button>Back</button></Link></p>
+            </form>
+        </div>
+    )
 }
-
-export default UploadImages;
--- my-react-app/src/pages/Profile.jsx
+++ my-react-app/src/pages/Profile.jsx
@@ -8,3 +8,59 @@ function Profile() {
 }
 
 export default Profile;
+
+import { useState, useEffect } from "react";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  getStorage,
+  listAll,
+  list,
+} from "firebase/storage";
+import { firebaseApp } from "./firebase";
+import { v4 } from "uuid";
+
+const storage = getStorage(firebaseApp); 
+
+function UploadImages() {
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageUrls, setImageUrls] = useState([]);
+
+  const imagesListRef = ref(storage, "images/");
+  const uploadFile = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
+    });
+  };
+
+  useEffect(() => {
+    listAll(imagesListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageUrls((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <input
+        type="file"
+        onChange={(event) => {
+          setImageUpload(event.target.files[0]);
+        }}
+      />
+      <button onClick={uploadFile}> Upload Image</button>
+      {imageUrls.map((url) => {
+        return <img src={url} height="300"/>;
+      })}
+    </div>
+  );
+}
+
