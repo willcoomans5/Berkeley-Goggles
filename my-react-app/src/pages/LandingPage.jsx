0b2e0fa6a9893bf19401bfbb1df54ef04ce0bc62
Rachel Cortez <rach.cort@berkeley.edu>
2023-12-06 15:35:34 -0800

    landing and about page done

--- my-react-app/src/pages/LandingPage.jsx
+++ my-react-app/src/pages/LandingPage.jsx
@@ -15,8 +15,51 @@ function LandingPage() {
         <p className = "signup"> <Link to="/signup">Sign up</Link></p>
         <div className = "about">
         <img src={logo} className = "logoImage" alt="logo" />
+
         <h2 className = "aboutTitle"> Who needs rose colored glasses when you have Berkeley Goggles? </h2>
-        <p className = "aboutPara"> Welcome to Berkeley Goggles, the #1 Dating App for UC Berkeley Students. </p>
+            <p className = "aboutPara"> Welcome to Berkeley Goggles – where 
+            connections go beyond the books! Created by UC Berkeley 
+            students for UC Berkeley students, our dating app is 
+            designed to bring together the brilliant minds and 
+            diverse personalities that make up our unique campus community. </p>
+
+        <h3 className = "aboutSubTitle"> 🌟 About Berkeley Goggles 🌟 </h3>
+            <p className = "aboutPara"> At Berkeley Goggles, we believe that finding meaningful 
+                connections should be as inspiring as a captivating lecture 
+                or a stimulating lab experiment. Embrace the chance to 
+                connect with fellow Bears who share your passions, 
+                ambitions, and a love for the Berkeley experience.</p>
+
+        <h3 className = "aboutSubTitle"> 🔍 See Beyond the Surface 🔍 </h3>
+            <p className = "aboutPara">
+                Ditch the superficial judgments and let Berkeley Goggles help you see 
+                beyond the surface. Our app encourages authentic connections by highlighting 
+                interests, hobbies, and shared experiences, 
+                ensuring you match with someone who gets you on a deeper level.</p>
+
+    <h3 className = "aboutSubTitle">📚 Exclusive to UC Berkeley 📚 </h3>
+        <p className = "aboutPara"> 
+        Created by students, for students – Berkeley Goggles 
+        is exclusively tailored to the UC Berkeley community. 
+        Whether you are studying at the Main Stacks, grabbing 
+        a coffee at Free Speech Movement Café, or cheering on the 
+        Golden Bears, our app is here to enhance your college dating experience.
+        </p>
+        <h3 className = "aboutSubTitle"> 🎓 Dating with Purpose 🎓 </h3>
+            <p className = "aboutPara"> 
+            Berkeley Goggles is more than just a dating app; 
+            it's a platform for meaningful connections. Whether 
+            you're looking for a study buddy, a partner-in-crime for 
+            exploring the Bay Area, or someone to share deep conversations with, 
+            find it all at Berkeley Goggles.
+
+        Join us on this exciting journey of discovery, connection, 
+        and love at UC Berkeley – where wearing Berkeley Goggles 
+        opens your eyes to a world of incredible possibilities!</p>
+        </div>
+        <div className = "footer">
+        <p> Contact Us!</p>
+        <p> berkeleygoggles@gmail.com </p>
         </div>
         </>
     )
