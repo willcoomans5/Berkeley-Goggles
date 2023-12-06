a1bd5dfdf170a7e1d825567584c19ecedec4c125
Rachel Cortez <rach.cort@berkeley.edu>
2023-12-06 11:41:25 -0800

    about section added to landing page

--- my-react-app/src/pages/LandingPage.jsx
+++ my-react-app/src/pages/LandingPage.jsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
-import './LandingPage.css'
+import background from '../assets/IMG_3232.svg';
+import logo from '../assets/berk-goggles-logo.svg';
+import './LandingPage.css';
 function LandingPage() {
     return (
         <>
-        <h1 className = "landingTitle"> BERKELEY GOGGLES </h1>
-        <p className = "signInButton"> <Link to="/login">Log in</Link></p>
-        <p className = "signUpButton"> <Link to="/signup">Sign up</Link></p>
+            <img src={background} className = "backgroundImage" alt="backgound-landing" />
+            <div>
+                <h1  className = "landingTitle"> BERKELEY GOGGLES </h1>
+        </div>
+        {/* <img src={background} alt="background-landing" /> */}
+        {/* <h1 className = "landingTitle"> BERKELEY GOGGLES </h1> */}
+        <p className = "login"> <Link to="/login">Log in</Link></p>
+        <p className = "signup"> <Link to="/signup">Sign up</Link></p>
+        <div className = "about">
+        <img src={logo} className = "logoImage" alt="logo" />
+        <h2 className = "aboutTitle"> Who needs rose colored glasses when you have Berkeley Goggles? </h2>
+        <p className = "aboutPara"> Welcome to Berkeley Goggles, the #1 Dating App for UC Berkeley Students. </p>
+        </div>
         </>
     )
 }
