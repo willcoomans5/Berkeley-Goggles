2b6b0a2f4f6eedc345675eff04a700a691768cd2
John Chang <jchang04@berkeley.edu>
2023-12-08 12:41:34 -0800

    display match info

--- my-react-app/src/database/Match.jsx
+++ my-react-app/src/database/Match.jsx
@@ -1,65 +1,51 @@
-// import React, { useEffect, useState } from 'react';
-// import { getDatabase, ref, get, child } from 'firebase/database';
-// import { firebaseApp } from './firebase';  // Update the import statement
-
-// const db = getDatabase(firebaseApp);
-
-// const Match = () => {
-//   const [data, setData] = useState(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const usersRef = ref(db, 'users.uid');
-
-//       try {
-//         const snapshot = await get(child(usersRef));
-
-//         if (snapshot.exists()) {
-//           setData(snapshot.val());
-//         } else {
-//           console.log('No data available');
-//         }
-//       } catch (error) {
-//         console.error('Error getting data', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div>
-//       {/* Render your component with the data */}
-//       {data && <p>{data}</p>}
-//     </div>
-//   );
-// };
-
-// export default Match;
-
-
 import React, { useEffect, useState } from 'react';
 import { collectionGroup, query, where, getDocs, getFirestore } from "firebase/firestore";  
 import { firebaseApp } from './firebase';
 
-const db = getFirestore(firebaseApp)
+const db = getFirestore(firebaseApp); 
 
-const Match = async() => {
-  // Query a reference to a subcollection
-  const matches = query(collectionGroup(db, 'users'));
-  const querySnapshot = await getDocs(matches);
-  querySnapshot.forEach((doc) => {
-    console.log(doc.id, ' => ', doc.data());
-  });
+const Match = () => {
+  const [matchlist, setMatchlist] = useState([]); 
+
+  async function match(e) {
+    e.preventDefault(); 
+    setMatchlist([]); 
+    // Query a reference to a subcollection
+    const matches = query(collectionGroup(db, 'users'));
+    const querySnapshot = await getDocs(matches);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, ' => ', doc.data());
+      setMatchlist(matchlist => [...matchlist, doc.data()]);
+      console.log(matchlist); 
+    });}
+  
+    const listItems = matchlist.map(person =>
+      <li>
+        <ul>
+          <li>{person.name}</li>
+          <li>{person.birthday}</li>
+          <li>{person.description}</li>
+        </ul>
+      </li>
+    );
 
   return (
+    <>
     <div className="matches">
       {/* Render your component with the data */}
       {(
         <div>
+          <ul>{listItems}</ul>;
         </div>
       )}
     </div>
+    <div>
+      <form onSubmit={match}>
+        <button type="submit">Display Matches</button>
+      </form>
+
+    </div>
+    </>
   );
 };
 
